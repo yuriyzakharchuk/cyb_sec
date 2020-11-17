7c50266c6114069d8cf5066c9aaeea42b60a0b4b
Yuriy Zakharchuk <y.a.zakharchuk@gmail.com>
2020-11-17 17:00:48 +0200

    changed samples and added computing coeffs program

---
+++ lab_4/affine_cipher/compute_coffs.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cstring>
+
+long
+gcd(long a, long b) 
+{ 
+ 	if (a == 0) 
+ 		return b; 
+ 	return gcd(b % a, a); 
+} 
+
+std::vector<long>
+get_all_coprime_ints(long m) {
+    std::vector<long> a;
+    for(int i = 2; i < m; ++i) {
+        if(gcd(i, m) == 1) {
+            a.push_back(i);
+        }
+    }
+    return a;
+}
+
+int
+main(int argc, char *argv[]) {
+    if(argc != 4) {
+        std::cerr << "Usage m freq_in_source freq_in_dest " << std::endl;
+        std::exit(1);
+    }
+    std::map<long, long> coeff_pairs;
+
+    long m { std::atol(argv[1]) };
+    long x { std::atol(argv[2]) };
+    long y { std::atol(argv[3]) };
+    for(auto a : get_all_coprime_ints(m)) {
+        for(int b = 0; b < m; ++b) {
+            if((x * a + b) % m == y) {
+                coeff_pairs.insert(std::pair<int, int>(a, b));
+            }
+        }
+    }
+    for(auto &x : coeff_pairs) {
+        std::cout << "a = " << x.first << " b = " << x.second << std::endl;
+    }
+    return 0;
+}
+
